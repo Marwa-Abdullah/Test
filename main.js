7c45190d1c9f5b09cb8c35faa320cc6fdff01019
Marwa-Abdullah <marwabdullah000@gmail.com>
2025-08-27 13:03:33 +0300

    testtt

--- main.js
+++ main.js
@@ -9,10 +9,10 @@
 // document.getElementById("containt").innerHTML = x+y;
 // document.getElementById("containt").innerHTML = x + c;
 
-let x = 5;
-let y = 20;
-let z = 3;
-let max = 0;
+// let x = 5;
+// let y = 20;
+// let z = 3;
+// let max = 0;
 
 // if (x > y && x > z) {
 //     max = x;
@@ -26,16 +26,56 @@ let max = 0;
 //     max = z;
 //     console.log(max);
 // }
-function mim(x, y, z) {
-    if (x > y && x > z) {
-      max = x;
-      return x
-    } else if (y > x && y > z) {
-      max = y;
-      return y
-    } else {
-      max = z;
-      return z
+// function mim(x, y, z) {
+//     if (x > y && x > z) {
+//       max = x;
+//       return x
+//     } else if (y > x && y > z) {
+//       max = y;
+//       return y
+//     } else {
+//       max = z;
+//       return z
+//     }
+// }
+// console.log(mim(2,8,5)*2)
+
+
+// let max = 0;
+// let i = 0;
+
+
+// function summing(x) {
+//   if (x < 0)
+//   {
+//     return 0 
+    
+//   }
+//   else {
+//     let sum = 0;
+//     let i = 1;
+//     while (i<=x) {
+//       sum +=i
+//     i++
+//     }
+//     return sum;
+//   }
+// }
+// console.log(summing(5));
+// ----------------------------------------------------------------
+  
+function isPrime(y) {
+  if (y<0) {
+    return false
+  }
+  else if(y>=i) {
+    let i = 2;
+    while (y % i === 0) {
+      return false;
     }
+  }
+  else {
+    return true
+  }
 }
-console.log(mim(2,8,5)*2)
+console.log(isPrime(6));
