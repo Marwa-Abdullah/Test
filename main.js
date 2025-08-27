64132b81aa69ef5409f924b2731f7b58d569776d
Marwa-Abdullah <marwabdullah000@gmail.com>
2025-08-27 09:40:10 +0300

    css+js test

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+// let x = 5;
+// let y = 3;
+// let c = "marwa";
+// // console.log(x + y);
+// // console.log(typeof x);
+// // console.log(typeof c);
+// document.getElementById("containt").innerHTML = x+y;
+// document.getElementById("containt").innerHTML = x + c;
+
